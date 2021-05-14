ca17c3531117224fa8dd3ba465d57788dac3de4d
Eben Burger <eben@arlowconsulting.co.za>
2021-05-14 15:44:29 +0200

    added next.config file to allow importing images

---
+++ hulu-clone/next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    images: {
+        // this allows us to use the images,
+        // this is how we can permit that next.js to use the images.
+        domains: ["links.papareact.com", "image.tmdb.org"]
+    },
+}
