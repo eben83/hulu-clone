534aa86b26c78d54af1b7ac2456d3c2e01ac2673
Eben Burger <eben@arlowconsulting.co.za>
2021-05-17 18:21:01 +0200

    added animation 'flip-move' to thumbnails

--- hulu-clone/components/Results.js
+++ hulu-clone/components/Results.js
@@ -1,14 +1,14 @@
 import Thumbnail from "./Thumbnail"
+import FlipMove from 'react-flip-move'
 
 function Results( { results } ) {
-    console.log(results)
     return (
-        <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 
+        <FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 
         3xl:flex flex-wrap justify-cenrter'>
             {results.map((result) => (
                 <Thumbnail key={result.id} result={result}/>
             ))}
-        </div>
+        </FlipMove>
     )
 }
 
