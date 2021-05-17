769897dbae948240ace71201217d1863b9f7c77c
Eben Burger <eben@arlowconsulting.co.za>
2021-05-17 18:04:03 +0200

    updated results and added custom screen size

--- hulu-clone/pages/index.js
+++ hulu-clone/pages/index.js
@@ -4,6 +4,7 @@ import Nav from '../components/Nav'
 import Results from '../components/Results'
 import requests from '../utils/requests'
 
+
 export default function Home({results}) {
   console.log(results)
   return (
@@ -28,14 +29,15 @@ export default function Home({results}) {
 export async function getServerSideProps(context) {
   const genre = context.query.genre;
 
-  const request = await fetch(`
-  https://api.themoviedb.org/3${requests[genre]?.url || 
-  requests.fetchTrending.url}`
-  ).then(res => res.json())
+  const request = await fetch (
+    `https://api.themoviedb.org/3${
+      requests.[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json())
 
   return {
     props: {
       results: request.results,
-    }
-  }
+    },
+  };
 }
